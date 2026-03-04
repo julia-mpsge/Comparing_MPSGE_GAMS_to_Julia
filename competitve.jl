fddd5230021860f68563324a6f1ab47ccc7d4193
Mitch Phillipson <mitch.phillipson@gmail.com>
2026-03-04 14:31:44 -0600

    Add MCP version of CompetitiveDirect

--- competitve.jl
+++ competitve.jl
@@ -1,9 +1,9 @@
 using MPSGE
 using DataFrames
 
-I = 1:9
-J = 1:11
-G = 1:11
+I = 1:3
+J = 1:3
+G = 1:3
 
 include("big/CompetitiveDirect.jl")
 using .CompetitiveDirect
@@ -12,6 +12,9 @@ M_direct = CompetitiveDirect.competitive_model(I, J, [:L, :K], G);
 
 solve!(M_direct)
 
+M_mcp = CompetitiveDirect.mcp_competitive_model(I, J, [:L, :K], G);
+
+optimize!(M_mcp)
 
 include("big/CompetitiveDirect_modify.jl")
 using .CompetitiveDirect_modify
@@ -20,17 +23,17 @@ M_modify = CompetitiveDirect_modify.competitive_model(I, J, [:L, :K], G);
 
 solve!(M_modify)
 
-begin
-    df = DataFrame()
-    for i=I, j=J, g=G
-        var_m = M_modify[:PX][i,j,g]
-        var_d = M_direct[:PX][i,j,g]
 
-        diff = value(var_m) - value(var_d)
-        if abs(diff) > 1e-10
-            #println("Difference: (i=$i, j=$j, g=$g): $(value(var_m)) vs $(value(var_d)) diff: $diff")
-            push!(df, (i=i, j=j, g=g, var_m = value(var_m), var_d = value(var_d), diff=diff))
-        end
-    end
-    df
-end
+var_m = M_modify[:PX]
+var_d = M_direct[:PX]
+var_mcp = M_mcp[:PX]
+
+df = DataFrame([
+    (i=i, j=j, g=g, var_m = value(var_m[i,j,g]), var_d = value(var_d[i,j,g]), var_mcp = value(var_mcp[i,j,g]))
+    for i=I, j=J, g=G
+])
+
+df |>
+    x -> subset(x,
+        [:var_d, :var_mcp] => ByRow((x,y) -> abs(x-y) > 1e-10)
+    )
